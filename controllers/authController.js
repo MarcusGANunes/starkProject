c16554b65ba36cee19e593a9a997523efe801678
Marcus Nunes <marcusnunes_11@hotmail.com>
2023-05-27 17:49:34 -0300

    crypto auth added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,31 @@
 const { WebClient } = require('@slack/web-api')
 require('dotenv').config()
 const token = process.env.SLACK_BOT_TOKEN
+const crypto = require('crypto-js')
 
 const slackClient = new WebClient(token)
 
 const sendCode = async (slackId) => {
-  const code = parseInt(Math.random() * 999999)
+  const code = parseInt(Math.random() * (999999 - 100000) + 100000)
   const msg = `Olá! Seu token de verificação é ${code}.`
   await slackClient.chat.postMessage({ channel: slackId, text: msg})
-  return code
+  return code.toString()
 }
 
 
-const validateCode = (code, inputCode) => code === inputCode
+const validateCode = (code, inputCode) => code === crypto.SHA256(inputCode).toString(crypto.enc.Base64)
 
 exports.sendVerificationCode = async (req, res, next) => {
   const slackId = req.body.slackId
 
   const code = await sendCode(slackId)
 
+  const hash = crypto.SHA256(code)
+
   res.status(200).json({
     success: true,
     message: 'Code sent',
+    hash: hash.toString(crypto.enc.Base64)
   })
 }
 
